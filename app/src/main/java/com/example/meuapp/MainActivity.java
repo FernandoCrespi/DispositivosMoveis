dd3a36f5aeaf753f9a33765a7bd99d228bb909d4
FernandoCrespi <flcrespi@outlook.com>
2026-04-09 11:09:42 -0300

    IMC

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -13,5 +13,19 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageview;
+    Button btnCalcular;
+    EditText peso, altura;
+    TextView resul,classi;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        imageview=findViewById(R.id.imageView);
+        btnCalcular=findViewById(R.id.button);
+        peso=findViewById(R.id.etPeso);
+        altura=findViewById(R.id.etAltura);
+        resul=findViewById(R.id.tvResul);
+        classi=findViewById(R.id.tvClass);
+
+    }
 }
