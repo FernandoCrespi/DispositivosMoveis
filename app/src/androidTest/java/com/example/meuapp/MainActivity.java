36b00f90b8ef324f6c20e40b5559c3aecc54923c
FernandoCrespi <flcrespi@outlook.com>
2026-06-17 11:50:51 -0300

    MuitoErrado

---
+++ app/src/androidTest/java/com/example/meuapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.meuapp;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textView);
+
+        SensorManager mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mSensorManager.registerListener(this,mSensor,SensorManager.SENSOR_DELAY_NORMAL
+        );
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+         textView.setText("valor do sensor de luz: "+event.values[0]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
